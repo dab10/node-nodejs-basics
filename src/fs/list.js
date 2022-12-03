5ee507a3d0dd818ec10b76aa3594a08dac83f98f
dab10 <dab10@list.ru>
2022-12-03 17:31:20 +0300

    feat: list.js done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { readdir } from 'node:fs/promises';
+
 const list = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  try {
+    const files = await readdir(path.join(__dirname, 'files'));
+    console.log(files);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
