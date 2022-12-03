84e7be2e0de3b8df2d76e99cdfed611b014f3024
dab10 <dab10@list.ru>
2022-12-04 00:09:17 +0300

    feat: read.js done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const fileForStream = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  const stream = createReadStream(fileForStream, 'utf-8');
+  
+  let data = '';
+  
+  stream.on('data', chunk => data += chunk);
+  stream.on('end', () => stdout.write(data));
+  stream.on('error', error => console.log('Error', error.message));
 };
 
 await read();
