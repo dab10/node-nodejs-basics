5b9c9ededfd58853c3dfe9d2ce60bb5d7510af49
dab10 <dab10@list.ru>
2022-12-04 02:24:15 +0300

    feat: compress.js done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,28 @@
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const fileForZip = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const fileZip = path.join(__dirname, 'files', 'archive.gz');
+
+  const gzip = createGzip();
+  const readStream = createReadStream(fileForZip);
+  const writeStream = createWriteStream(fileZip);
+
+  pipeline(
+    readStream, 
+    gzip, 
+    writeStream, 
+    (err) => {
+      if (err) {
+        console.log('An error occurred:', err);
+      }
+    }
+  );
 };
 
 await compress();
